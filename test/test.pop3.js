d4ef144dc57dd664e059770bf5f7a0e53c975a41
TouSS <dxiaoxiang@gmail.com>
2019-01-14 17:27:41 +0800

    pop3 command ...

--- test/test.pop3.js
+++ test/test.pop3.js
@@ -1,37 +1,35 @@
-const POP3 = require('../lib/pop3')
+const { POP3 } = require('../lib/pop3')
 
-const simpleParser = require("mailparser").simpleParser
+const simpleParser = require('mailparser').simpleParser
 
 let client = new POP3()
 
 client.multiResp = false
 client
-  .connect('pop3.zltel.com.cn')
+  .connect('host')
   .then(resp => {
     //console.log(resp)
     client
-      ._sendCommand('USER', 'dengxx@zltel.com.cn')
+      .user('username')
       .then(resp => {
         console.log(resp)
         client
-          ._sendCommand('PASS', '@toush00')
+          .pass('password')
           .then(resp => {
             console.log(resp)
-            client.multiResp = true
-            client.respEnd = false
             client
-              ._sendCommand('retr', '1')
+              .retr('232')
               .then(resp => {
                 simpleParser(resp)
-                  .then(mail => {console.log(mail)})
-                  .catch(err => {console.error(err)})
-                client.multiResp = false
-                client.respEnd = false
-                client
-                  ._sendCommand('QUIT')
-                  .then(resp => {
-                    console.log(resp)
+                  .then(mail => {
+                    console.log(mail)
                   })
+                  .catch(err => {
+                    console.error(err)
+                  })
+                client.quit().then(resp => {
+                  console.log(resp)
+                })
               })
               .catch(err => {
                 console.error(err)
